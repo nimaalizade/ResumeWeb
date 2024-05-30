cfe4bdef3682c2e82e4a6c920179a0799366ac45
nimaz <nimaalizadeh13844@gmail.com>
2024-05-30 23:59:25 +0330

    adding basic navbar and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'times-new-romance': ['"TimesNewRomance"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
