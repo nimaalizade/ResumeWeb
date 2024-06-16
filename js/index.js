9a1f418708350077f2be57ae402e375b8db335c0
nimaz <126095883+nimaalizade@users.noreply.github.com>
2024-06-16 16:04:16 +0330

    add section skill's

--- js/index.js
+++ js/index.js
@@ -1,15 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const typedOut = document.querySelector('.animate-typewriter');
-  
-    typedOut.innerHTML = '';
-  
-    const text = 'Hello!'
-    let charIndex = 0;
-  
-    setInterval(() => {
-      if (charIndex < text.length) {
-        typedOut.innerHTML += text[charIndex];
-        charIndex++;
-      }
-    }, 100);
+document.addEventListener("DOMContentLoaded", () => {
+  const typedOut = document.querySelector(".animate-typewriter");
+
+  typedOut.innerHTML = "";
+
+  const text = "Hello!!";
+  let charIndex = 0;
+
+  setInterval(() => {
+    if (charIndex < text.length) {
+      typedOut.innerHTML += text[charIndex];
+      charIndex++;
+    }
+  }, 200);
+});
+document.querySelectorAll('a[href*="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
   });
+});
